b1f5d07ab5de430404b5cad274e26d38b4f53f20
guyunxiang <guyunxiang32@gmail.com>
2024-07-18 11:30:34 -0400

    update home styles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,43 +1,95 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-import reactLogo from '../assets/react.svg';
 import blockletLogo from '../assets/blocklet.svg';
-import viteLogo from '../assets/vite.svg';
 import plusIcon from '../assets/plus-lg.svg';
 
 import './home.css';
 
 import api from '../libs/api';
 
-function Home() {
-  const [count, setCount] = useState(0);
+// Define the structure of API responses
+interface ApiResponse<T> {
+  success: boolean;
+  message?: string;
+  data?: T;
+}
 
-  async function getApiData() {
-    const { data } = await api.get('/api/data');
-    const { message } = data;
-    alert(`Message from api: ${message}`);
-  }
+type BasicInfo = string[];
+
+interface ProfileItem {
+  _id: string;
+  name: string;
+  basicInfo: BasicInfo;
+}
+
+type ProfileList = ProfileItem[];
+
+function Home() {
+  const [walletAddress, setWalletAddress] = useState<string>('');
+  const [profileList, setProfileList] = useState<ProfileList>([]);
+
+  // Fetch client wallet address as primary key
+  useEffect(() => {
+    const fetchWalletAddress = async () => {
+      try {
+        const res = await api.get<ApiResponse<string>>('/api/address/query');
+        const { success, data } = res.data;
+        if (!success || !data) {
+          toast.error('Failed to obtain the current wallet address!');
+          return;
+        }
+        setWalletAddress(data);
+      } catch (error) {
+        toast.error('An error occurred while fetching wallet address');
+      }
+    };
+    fetchWalletAddress();
+  }, []);
+
+  useEffect(() => {
+    const fetchProfileList = async (wallet: string) => {
+      try {
+        const res = await api.get<ApiResponse<ProfileList>>(`/api/profiles/query?wallet=${wallet}`);
+        const { success, data } = res.data;
+        if (success && data) {
+          setProfileList(data);
+        }
+      } catch (error) {
+        toast.error('An error occurred while fetching profile data');
+      }
+    };
+    if (walletAddress) {
+      fetchProfileList(walletAddress);
+    }
+  }, [walletAddress]);
 
   return (
     <div className="home-page container">
-      <div className="text-center">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="d-flex align-items-center justify-content-between gap-3">
+        <h1>Resume manager</h1>
         <a href="https://www.arcblock.io/docs/blocklet-developer/getting-started" target="_blank" rel="noreferrer">
           <img src={blockletLogo} className="logo blocklet" alt="Blocklet logo" />
         </a>
       </div>
-      <h1 className="mb-3">Resume Manager</h1>
+      <hr className="mb-3 mb-md-5" />
       <div className="card">
         <ul className="resume-list">
-          <li>
-            <Link to="/profile">profile</Link>
-          </li>
+          {profileList.map(({ _id, name, basicInfo }) => (
+            <li key={_id}>
+              <Link to={`/profile/${_id}`}>
+                <h3 className="mb-3">{name}</h3>
+                <div className="d-flex flex-column gap-2">
+                  {basicInfo.slice(0, 3).map((item) => (
+                    <p key={item} className="m-0">
+                      {item}
+                    </p>
+                  ))}
+                </div>
+              </Link>
+            </li>
+          ))}
           <li className="create_new">
             <Link to="/profile/new">
               <img src={plusIcon} alt="add" className="add" />
